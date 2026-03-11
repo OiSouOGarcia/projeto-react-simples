eeddd59b22398a39902f9749fd0c205949aa05df
João Garcia <joaogarcia8038@gmail.com>
2026-03-11 15:52:00 -0300

    commit inicial

---
+++ src/App.jsx
@@ -0,0 +1,55 @@
+import Tasks from "./assets/Components/Tasks";
+import AddTask from "./assets/Components/AddTask";
+import { useState } from "react";
+
+function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      title: "Estudar Programação",
+      description:
+        "Estudar programação para se tornar um desenvolver full stack.",
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      title: "Estudar Ingles",
+      description: "Estudar Ingles para se tornar um desenvolver full stack.",
+      isCompleted: false,
+    },
+    {
+      id: 3,
+      title: "Estudar Matematica",
+      description:
+        "Estudar Matematica para se tornar um desenvolver full stack.",
+      isCompleted: false,
+    },
+  ]);
+
+  function onTaskClick(taskId) {
+    const newTasks = tasks.map((task) => {
+      if (task.id === taskId) {
+        return { ...task, isCompleted: !task.isCompleted };
+      }
+      return task;
+    });
+
+    setTasks(newTasks);
+  }
+
+  return (
+    <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
+      <div className="w-[500px]">
+        <h1 className="text-3xl text-slate-100 font-bold text-center">
+          Gerenciador De Tarefas
+        </h1>
+
+        <AddTask />
+
+        <Tasks tasks={tasks} onTaskClick={onTaskClick} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
